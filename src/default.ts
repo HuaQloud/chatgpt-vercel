fc313662dcb6c0240b0e6165f9d9dc515cca483a
HuaQloud <128353421+HuaQloud@users.noreply.github.com>
2023-04-26 14:44:18 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,5 +8,5 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - 本网站基于 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 技术，仅用于演示。
-- 本站预计五一之后进行迁移，新站地址届时请咨询站长。
+- 本站预计五一之后进行迁移，新站地址届时请咨询站长，感谢大家的陪伴。
 - 输入框中输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索内置角色。<kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。`
